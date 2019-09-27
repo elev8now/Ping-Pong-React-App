c829ba30eec99de2977e4a06c42fdb980b6ddff7
Jonathan Smithies <jonathansmithies@Jonathans-iMac.local>
2019-09-28 00:38:16 +0100

    Updates

---
+++ precache-manifest.921b6d16c317bd06490a91b74212ba5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28484e2346ceea93ffadd9b725264f0",
+    "url": "/ping-pong/index.html"
+  },
+  {
+    "revision": "f7042eb5f116bb79a6e6",
+    "url": "/ping-pong/static/css/main.f35d2220.chunk.css"
+  },
+  {
+    "revision": "3e67cbcc21a858af908e",
+    "url": "/ping-pong/static/js/2.580a0e73.chunk.js"
+  },
+  {
+    "revision": "f7042eb5f116bb79a6e6",
+    "url": "/ping-pong/static/js/main.99e2eb12.chunk.js"
+  },
+  {
+    "revision": "082485cf2c513ef22458",
+    "url": "/ping-pong/static/js/runtime-main.38245283.js"
+  },
+  {
+    "revision": "c75ef5c0f519a4f3011a5deb758cbe29",
+    "url": "/ping-pong/static/media/flow-2.c75ef5c0.jpg"
+  },
+  {
+    "revision": "27698f87592fe0a12112b58619b26067",
+    "url": "/ping-pong/static/media/ping-pong-table.27698f87.png"
+  }
+]);
