ef363dd52a2187e7c95a430f1dced6e85ecafd42
Jonathan Smithies <jonathansmithies@Jonathans-iMac.local>
2019-09-28 00:46:23 +0100

    Updates

---
+++ precache-manifest.73526538ef677bb7269bd3459ac6c27a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1db705e22515fa9bdeb285fbe019cd",
+    "url": "/Ping-Pong-React-App/index.html"
+  },
+  {
+    "revision": "f7042eb5f116bb79a6e6",
+    "url": "/Ping-Pong-React-App/static/css/main.f35d2220.chunk.css"
+  },
+  {
+    "revision": "3e67cbcc21a858af908e",
+    "url": "/Ping-Pong-React-App/static/js/2.580a0e73.chunk.js"
+  },
+  {
+    "revision": "f7042eb5f116bb79a6e6",
+    "url": "/Ping-Pong-React-App/static/js/main.99e2eb12.chunk.js"
+  },
+  {
+    "revision": "24d8f06df7acf4838db5",
+    "url": "/Ping-Pong-React-App/static/js/runtime-main.b7ba8dc2.js"
+  },
+  {
+    "revision": "c75ef5c0f519a4f3011a5deb758cbe29",
+    "url": "/Ping-Pong-React-App/static/media/flow-2.c75ef5c0.jpg"
+  },
+  {
+    "revision": "27698f87592fe0a12112b58619b26067",
+    "url": "/Ping-Pong-React-App/static/media/ping-pong-table.27698f87.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ping-pong/precache-manifest.921b6d16c317bd06490a91b74212ba5e.js"
+  "/Ping-Pong-React-App/precache-manifest.73526538ef677bb7269bd3459ac6c27a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ping-pong/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ping-Pong-React-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
